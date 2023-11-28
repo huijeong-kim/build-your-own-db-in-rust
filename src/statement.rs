e9c339ca5b9a9d34104962acd01ea2a3916927b6
Huijeong Kim <herehuijeong@gmail.com>
2023-11-28 16:34:49 +0900

    refactoring

--- src/statement.rs
+++ src/statement.rs
@@ -5,6 +5,50 @@ pub enum Statement {
     Insert(Row),
     Select,
 }
+impl Statement {
+    pub fn new(args: &Vec<&str>) -> Result<Self, PrepareResult> {
+        let command = args[0];
+        match command {
+            "insert" => Statement::new_insert(args),
+            "select" => Statement::new_select(args),
+            _ => Err(PrepareResult::UnrecognizedCommand)
+        }
+    }
+    fn new_insert(args: &Vec<&str>) -> Result<Self, PrepareResult> {
+        if args.len() < 4 {
+            return Err(PrepareResult::SyntaxError);
+        }
+
+        let mut row = Row::new();
+        let id = args[1].parse::<i32>().unwrap();
+        if id < 0 {
+            return Err(PrepareResult::NegativeId);
+        }
+        row.id = id as u32;
+
+        let username_len = args[2].as_bytes().len();
+        if username_len > COLUMN_USERNAME_SIZE {
+            return Err(PrepareResult::StringTooLong);
+        }
+        row.username[..username_len].copy_from_slice(args[2].as_bytes());
+
+        let email_len = args[3].as_bytes().len();
+        if email_len > COLUMN_EMAIL_SIZE {
+            return Err(PrepareResult::StringTooLong);
+        }
+        row.email[..email_len].copy_from_slice(args[3].as_bytes());
+
+        Ok(Statement::Insert(row))
+    }
+
+    fn new_select(args: &Vec<&str>) -> Result<Self, PrepareResult> {
+        if args.len() < 1 {
+            return Err(PrepareResult::SyntaxError);
+        }
+
+        Ok(Statement::Select)
+    }
+}
 
 pub enum PrepareResult {
     Success(Statement),
@@ -27,41 +71,9 @@ impl PrepareResult {
 
 pub fn prepare_statement(buffer: &String) -> PrepareResult {
     let args: Vec<&str> = buffer.split(' ').collect();
-    if args[0] == "insert" {
-        // insert id username email
-        if args.len() < 4 {
-            return PrepareResult::SyntaxError;
-        }
-
-        let mut row = Row::new();
-        let id = args[1].parse::<i32>().unwrap();
-        if id < 0 {
-            return PrepareResult::NegativeId;
-        }
-        row.id = id as u32;
-
-        let username_len = args[2].as_bytes().len();
-        if username_len > COLUMN_USERNAME_SIZE {
-            return PrepareResult::StringTooLong;
-        }
-        row.username[..username_len].copy_from_slice(args[2].as_bytes());
-
-        let email_len = args[3].as_bytes().len();
-        if email_len > COLUMN_EMAIL_SIZE {
-            return PrepareResult::StringTooLong;
-        }
-        row.email[..email_len].copy_from_slice(args[3].as_bytes());
-
-        PrepareResult::Success(Statement::Insert(row))
-    } else if args[0] == "select" {
-        // select id username email
-        if args.len() < 1 {
-            return PrepareResult::SyntaxError;
-        }
-
-        PrepareResult::Success(Statement::Select)
-    } else {
-        PrepareResult::UnrecognizedCommand
+    match Statement::new(&args) {
+        Ok(statement) => PrepareResult::Success(statement),
+        Err(err) => err,
     }
 }
 
